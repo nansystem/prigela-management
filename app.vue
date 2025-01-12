1ea5dd068663ca498a4b458043cfe93576d2ee13
nansystem <sato171786@gmail.com>
2025-01-12 17:01:59 +0900

    feat: ログイン機能を追加

--- app.vue
+++ app.vue
@@ -2,6 +2,14 @@
   <div>
     <header>
       <h1>アプリケーション共通のヘッダー</h1>
+      <div v-if="authStore.loading">Loading...</div>
+      <div v-else>
+        <div v-if="authStore.isAuthenticated">
+          <p>Welcome, {{ authStore.user?.email }}</p>
+          <button @click="authStore.signOut()">Sign out</button>
+        </div>
+        <GoogleLogin v-else />
+      </div>
     </header>
     <NuxtPage />
     <footer>
@@ -9,3 +17,13 @@
     </footer>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  await authStore.initialize()
+})
+</script>

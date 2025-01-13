eafe6de84213f95939a3109f70d0c3864ef6b4e1
nansystem <sato171786@gmail.com>
2025-01-13 15:58:00 +0900

    refactor: 追加、削除モーダルを用意

---
+++ components/AddMaterialModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <Modal v-model="isOpen">
+    <template #title>材料を追加</template>
+    <MaterialForm @success="closeModal" />
+  </Modal>
+</template>
+
+<script setup lang="ts">
+  import { ref, watch } from 'vue'
+  import Modal from './Modal.vue'
+  import MaterialForm from './MaterialForm.vue'
+
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(['update:modelValue'])
+
+  const isOpen = ref(props.modelValue)
+
+  const closeModal = () => {
+    isOpen.value = false
+    emit('update:modelValue', false)
+  }
+
+  watch(
+    () => props.modelValue,
+    newVal => {
+      isOpen.value = newVal
+    }
+  )
+</script>
--- components/MaterialList.vue
+++ components/MaterialList.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="space-y-4">
-    <!-- 材料追加ボタン -->
     <button
       class="w-full p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
       @click="isAddModalOpen = true"
@@ -8,13 +7,14 @@
       材料を追加
     </button>
 
-    <!-- 材料追加モーダル -->
-    <Modal v-model="isAddModalOpen">
-      <template #title>材料を追加</template>
-      <MaterialForm @success="isAddModalOpen = false" />
-    </Modal>
-
-    <!-- 材料一覧テーブル -->
+    <AddMaterialModal v-model="isAddModalOpen" />
+    <!-- 削除確認モーダル -->
+    <DeleteConfirmationModal
+      v-model="isDeleteModalOpen"
+      message="本当に削除しますか"
+      @confirm="confirmDelete"
+      @cancel="cancelDelete"
+    />
     <MaterialTable
       :materials="materialsStore.materials"
       :editing-indexes="editingIndexes"
@@ -23,21 +23,13 @@
       @save-edit="saveEdit"
       @remove-material="removeMaterial"
     />
-
-    <!-- 削除確認モーダル -->
-    <DeleteConfirmationModal
-      v-model="isDeleteModalOpen"
-      :message="'本当にこの材料を削除しますか？'"
-      @confirm="confirmDelete"
-    />
   </div>
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue'
-  import Modal from './Modal.vue'
-  import MaterialForm from './MaterialForm.vue'
   import MaterialTable from './MaterialTable.vue'
+  import AddMaterialModal from './AddMaterialModal.vue'
   import DeleteConfirmationModal from './DeleteConfirmationModal.vue'
   import { useMaterialsStore } from '~/stores/materials'
   import type { Material } from '~/stores/materials'
@@ -53,11 +45,7 @@
   // 初期データ取得
   materialsStore.fetchMaterials()
 
-  // リアルタイム更新の設定
-  onMounted(() => {
-    materialsStore.subscribeToChanges()
-  })
-
+  // 編集関連関数
   const startEditing = (index: number) => {
     editingIndexes.value.push(index)
     editingMaterials.value[index] = { ...materialsStore.materials[index] }
@@ -85,5 +73,10 @@
       deletingIndex.value = null
       isDeleteModalOpen.value = false
     }
+  }
+
+  const cancelDelete = () => {
+    deletingIndex.value = null
+    isDeleteModalOpen.value = false
   }
 </script>
